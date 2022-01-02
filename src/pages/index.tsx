7ab77effcc493dff0985120f646fa0eb6b6bc6f9
pgm-jeroendewelde <Jeroen.Dewelde@student.arteveldehs.be>
2022-01-02 23:09:32 +0100

    added graphql-tester, added local & external database, modifed readme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,65 +1,35 @@
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-
-import { gql } from "@apollo/client";
 import client from "../../apollo-client";
 
-// const Home: NextPage = ({ teachers }) => {
-export default function Home({ teachers }: any) {
-  // teachers = teachers.teachers.data;
+import CourseList from "../components/Course/CourseList";
 
-  console.log("teachers...", teachers);
-  // console.log('test....', test)
+import { GET_ALL_COURSES } from "../../graphql/courses";
+import { Course } from "../../interfaces";
+
+interface HomeProps {
+    courses: Course[];
+}
+
+export default function Home({courses}: HomeProps) {
 
   return (
     <>
-      {teachers &&
-        teachers.map((teacher: any) => (
-          <div key={teacher.id}>
-            <h2>
-              {teacher.attributes.firstname ?? "no name"}{" "}
-              {teacher.attributes.lastname ?? ""}
-            </h2>
-          </div>
-        ))}
+      <CourseList courses={courses} />
     </>
   );
 }
 
 export async function getStaticProps() {
-  const { data, error } = await client.query({
-    query: gql`
-      {
-        teachers {
-          data {
-            attributes {
-              firstname
-              lastname
-            }
-          }
-        }
-      }
-    `,
-  });
+    const { data, error } = await client.query({
+        query: GET_ALL_COURSES
+    });
 
-  // console.log(data.teachers.data);
-  if (error) {
-    console.log(error);
-  }
+    if (error) {
+        console.log(error);
+    }
 
-  return {
-    props: {
-      teachers: data.teachers.data,
-      test: "hello",
-    },
-  };
+    return {
+        props: {
+            courses: data.courses,
+        },
+    };
 }
-
-// async function getData(url: string) {
-//     const res = await fetch(url);
-//     const data = await res.json();
-//     return data;
-// }
