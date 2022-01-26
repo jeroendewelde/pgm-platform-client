2b598268740dc888fb56d8d27eae659872b12c5a
pgm-jeroendewelde <Jeroen.Dewelde@student.arteveldehs.be>
2022-01-26 22:32:28 +0100

    re-added Course folder in components

---
+++ src/components/Course/CourseProjects.tsx
@@ -0,0 +1,52 @@
+import { motion } from "framer-motion";
+import React from "react";
+import { useInView } from "react-intersection-observer";
+import styled from "styled-components";
+import { CourseClient } from "../../../interfaces";
+import useInViewObserver from "../../hooks/useInView";
+import { Card } from "../Project";
+import { GlitchTitle } from "../Titles/GlitchTitle";
+
+const Container = styled(motion.section)`
+  padding-top: 5rem;
+`;
+
+const ProjectsList = styled.ul`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+
+  margin-top: 3rem;
+`;
+
+interface CourseProjectsProps {
+  course: CourseClient;
+}
+
+const CourseProjects = ({ course }: CourseProjectsProps) => {
+  const { ref, inView } = useInView();
+  const animation = useInViewObserver(inView);
+
+  return (
+    <>
+      {course.projects && course.projects.length > 0 ? (
+        <Container ref={ref} animate={animation}>
+          <>
+            <GlitchTitle>Projecten</GlitchTitle>
+            <ProjectsList>
+              {course.projects.map((project) => (
+                <Card key={project.id} project={project} />
+              ))}
+            </ProjectsList>
+          </>
+        </Container>
+      ) : (
+        <></>
+      )}
+    </>
+  );
+};
+
+export default CourseProjects;
---
+++ src/components/Course/Filter.tsx
@@ -0,0 +1,150 @@
+import React, { useEffect, useState } from "react";
+import { Field, Formik } from "formik";
+import { MdExpandMore } from "react-icons/md";
+
+import styled from "styled-components";
+import { transparentize } from "polished";
+import { Select } from "./Select";
+import { LearningLine } from "../../../interfaces";
+
+interface Props {
+  open: boolean;
+}
+
+const Container = styled.div`
+  overflow: hidden;
+  display: flex;
+  width: 100%;
+  min-width: 17rem;
+  border-bottom: ${(props) => props.theme.colors.turquoise} 2px solid;
+  max-width: 18.5rem;
+`;
+
+const OpenSelect = styled.div<Props>`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  cursor: pointer;
+  margin-bottom: 0.5rem;
+  width: 100%;
+
+  .label {
+    font-weight: ${(props) => props.theme.fontWeights.bold};
+    font-size: ${(props) => props.theme.fontSizes.normal};
+  }
+
+  .icon {
+    transform: ${({ open }) => (open ? "rotate(180deg)" : "0")};
+    transition: ${(props) => props.theme.transition.normal};
+    font-size: ${(props) => props.theme.fontSizes.semimedium};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`;
+
+const FormContainer = styled.div<Props>`
+  overflow: hidden;
+  position: absolute;
+  z-index: 2;
+
+  min-height: ${({ open }) => (open ? "16rem" : "0")};
+  height: ${({ open }) => (open ? "16rem" : "0")};
+  transition: ${(props) => props.theme.transition.bounce};
+  margin-top: 2.5rem;
+
+  .form {
+    margin-top: -2.5rem;
+    transition: ${(props) => props.theme.transition.bounce};
+    transform: ${({ open }) =>
+      open ? "translateY(2.5rem)" : "translateY(-100%)"};
+    width: calc(100% - 1.5rem);
+    min-width: 17rem;
+    max-width: 20rem;
+    border-radius: ${(props) => props.theme.borderRadius.small};
+    overflow: hidden;
+  }
+`;
+
+interface SelectItemProps {
+  learningLines: LearningLine[];
+  onChange: (value: string) => void;
+}
+
+const Filter = ({ learningLines, onChange }: SelectItemProps) => {
+  const [open, setOpen] = useState(false);
+  const [selected, setSelected] = useState("");
+
+  const handleToggle = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    e.preventDefault();
+    setOpen(!open);
+  };
+
+  useEffect(() => {
+    onChange(selected);
+  }, [selected, onChange]);
+
+  return (
+    <Container>
+      <OpenSelect open={open} onClick={handleToggle}>
+        <span className="label">Leerlijn</span>
+        <span className="icon">
+          <MdExpandMore />
+        </span>
+      </OpenSelect>
+
+      <FormContainer open={open}>
+        <Formik
+          initialValues={{
+            leerlijn: "",
+          }}
+          onSubmit={(value) => {
+            setSelected(value.leerlijn);
+          }}
+        >
+          {({
+            values,
+            handleSubmit,
+            isSubmitting,
+            handleChange,
+            handleBlur,
+            submitForm,
+          }) => (
+            <form
+              className="form"
+              onClick={(e) => {
+                handleChange(e);
+                setTimeout(() => {
+                  submitForm();
+                });
+              }}
+            >
+              <Field
+                type="button"
+                name="leerlijn"
+                open={open}
+                value={"Alle"}
+                as={Select}
+                onClick={handleToggle}
+              />
+              {learningLines?.map((learningLine) => (
+                <Field
+                  key={`${learningLine.name}-${learningLine.id}`}
+                  type="button"
+                  name="leerlijn"
+                  open={open}
+                  value={learningLine.name}
+                  as={Select}
+                  onClick={handleToggle}
+                  color={learningLine.color}
+                />
+              ))}
+            </form>
+          )}
+        </Formik>
+      </FormContainer>
+    </Container>
+  );
+};
+
+export default Filter;
