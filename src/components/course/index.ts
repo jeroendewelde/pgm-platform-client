f314c6c939509ba1fdd785ea971a7adb7b3d80a1
pgm-jeroendewelde <Jeroen.Dewelde@student.arteveldehs.be>
2022-01-26 22:31:42 +0100

    removed Course folder in components

--- src/components/course/index.ts
+++
@@ -1,6 +0,0 @@
-import Card from "./Card";
-import HeroCourse from "./HeroCourse";
-import DocentenCourse from "./DocentenCourse";
-import CourseProjects from "./CourseProjects";
-
-export { Card, HeroCourse, DocentenCourse, CourseProjects };
