6215e2ded0f960b32478918b7795ebe4236fade4
pgm-jeroendewelde <Jeroen.Dewelde@student.arteveldehs.be>
2022-01-26 21:34:52 +0100

    temprarily commented out Tag in HeroCourse

--- src/components/Course/HeroCourse.tsx
+++ src/components/Course/HeroCourse.tsx
@@ -5,7 +5,7 @@ import styled from "styled-components";
 import test from "../../assets/test/test.jpg";
 
 import { CourseClient } from "../../../interfaces";
-import Tag from "./Tag";
+// import Tag from "./Tag";
 import { CourseTitle } from "../Titles/CourseTitle";
 import CTALink from "./CTALink";
 import { motion } from "framer-motion";
@@ -100,7 +100,8 @@ const HeroCourse = ({ course }: HeroCourseProps) => {
         {course.tags && course.tags.length > 0 && (
           <div className="tags">
             {course.tags.map((tag) => (
-              <Tag key={tag}>{tag}</Tag>
+              //   <Tag key={tag}>{tag}</Tag>
+              <div>{tag}</div>
             ))}
           </div>
         )}
